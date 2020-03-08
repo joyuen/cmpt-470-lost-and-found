d3f3d83d7f8a183b1b1f837b1118fcf88dddb31f
pnw1 <pnw1@sfu.ca>
2020-03-08 13:39:57 -0700

    Remove testing endpoints

--- routes.js
+++ routes.js
@@ -1,12 +1,7 @@
 const DB_NAME = "test";
 
 module.exports = function(app, client, templates) {
-    const db = client.db(DB_NAME);
-
-    // temporary endpoints
-    app.use('/double', require('./server/double'));
-    app.use('/mongo', require('./server/mongo')(db));
-    
+    const db = client.db(DB_NAME);    
     app.use('/postings', require('./server/postings')(db, templates));
     app.use('/submit-posting', require('./server/submit-posting')(db));
 };
