d3aed4b27094f8fe8b05f2feba30a3d16fcd7a29
elvinw <elvinw@sfu.ca>
2020-03-09 22:26:09 -0700

    Google map prototype
    Added campus selection

---
+++ server/map.js
@@ -0,0 +1,90 @@
+// This is the minimum zoom level that we'll allow
+let minZoomLevel = 16;
+
+function initMap() {
+    // GLOBALS
+
+    // SFU Burnaby
+    burnabyMinZoom = 16;
+    burnabyCenter = new google.maps.LatLng(49.2767988, -122.9169812);
+    burnabyBounds = new google.maps.LatLngBounds(
+         new google.maps.LatLng(49.272003, -122.933773),  // South West
+         new google.maps.LatLng(49.282021, -122.902325)   // North East
+    );
+
+    // SFU Vancouver
+    vancouverMinZoom = 18;
+    vancouverCenter = new google.maps.LatLng(49.284526, -123.111648);
+    vancouverBounds = new google.maps.LatLngBounds(
+         new google.maps.LatLng(49.284213, -123.113048),  // South West
+         new google.maps.LatLng(49.285356, -123.111055)   // North East
+    );
+
+    // SFU Surrey
+    surreyMinZoom = 17.5
+    surreyCenter = new google.maps.LatLng(49.18665,-122.8494658);
+    surreyBounds = new google.maps.LatLngBounds(
+         new google.maps.LatLng(49.185315, -122.852098),  // South West
+         new google.maps.LatLng(49.190122, -122.845559)   // North East
+    );
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        disablePanMomentum: true,
+        zoom: burnabyMinZoom,
+        minZoom: burnabyMinZoom,
+        center: new google.maps.LatLng(49.2767988, -122.9169812),
+        restriction: {latLngBounds: burnabyBounds, strictBounds: false},
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        clickableIcons: false,
+        styles: [
+                    {
+                        "featureType": "poi",
+                        "stylers": [{ "visibility": "off" }]
+                    },
+                    {
+                        "featureType": "poi.school",
+                        "stylers": [{ "visibility": "on" }]
+                    },
+                    {
+                        "featureType": "poi.government",
+                        "stylers": [{ "visibility": "on" }]
+                    },
+                    {
+                        "featureType": "poi.medical",
+                        "stylers": [{ "visibility": "on" }]
+                    },
+                    {
+                        "featureType": "poi.park",
+                        "stylers": [{ "visibility": "on" }]
+                    },
+                    {
+                        "featureType": "poi.sports_complex",
+                        "stylers": [{ "visibility": "on" }]
+                    }
+                ]
+    });
+
+    // Add controls to the map, allowing users to hide/show features.
+    var styleControl = document.getElementById('floating-panel');
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(styleControl);
+
+}
+
+function showBurnaby() {
+    map.setOptions({center: burnabyCenter, zoom: burnabyMinZoom, minZoom: burnabyMinZoom, restriction: {latLngBounds: burnabyBounds, strictBounds: false}})
+    map.setCenter(burnabyCenter);
+}
+
+function showVancouver() {
+    map.setOptions({center: vancouverCenter, zoom: vancouverMinZoom, minZoom: vancouverMinZoom, restriction: {latLngBounds: vancouverBounds, strictBounds: false}})
+    map.setCenter(vancouverCenter);
+}
+
+function showSurrey() {
+    map.setOptions({center: surreyCenter, zoom: surreyMinZoom, minZoom: surreyMinZoom, restriction: {latLngBounds: surreyBounds, strictBounds: false}})
+    map.setCenter(surreyCenter);
+}
+
+// initMap();
+// showBurnaby();
+
